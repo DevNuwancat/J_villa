1581042825abddeb5d73a2037c57d8488db1476d
Shashika <shashika90nuwan@gmail.com>
2026-07-31 07:51:37 +0530

    mobile issues fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import WhatsAppButton from './components/WhatsAppButton.vue'
 </script>
 
 <template>
   <RouterView />
+  <WhatsAppButton />
 </template>
